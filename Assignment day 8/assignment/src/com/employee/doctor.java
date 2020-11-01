a28f2d54d09588e3a2a770668c1f78faa649b3a7
Abhishek Chaudhary <70179559+abhishekfac@users.noreply.github.com>
2020-11-01 06:42:35 +0530

    Add files via upload

---
+++ Assignment day 8/assignment/src/com/employee/doctor.java
@@ -0,0 +1,8 @@
+package com.employee;
+
+public class doctor extends employee {
+    void treats(){
+        System.out.println("A doctor examine your health");
+
+    }
+}
---
+++ Assignment day 8/assignment/src/com/employee/engineer.java
@@ -0,0 +1,8 @@
+package com.employee;
+
+public class engineer extends employee {
+    void build(){
+        System.out.println("An engineer build things for us");
+
+    }
+}
