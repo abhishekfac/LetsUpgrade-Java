d9a1298ede5eec950fc1409f8fa97c3f5b711d13
Abhishek Chaudhary <70179559+abhishekfac@users.noreply.github.com>
2020-11-01 06:23:26 +0530

    Add files via upload

---
+++ Assignment day 8/question 1/assignment/src/com/employee/Main.java
@@ -0,0 +1,11 @@
+package com.employee;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        employee e =new employee();
+        e.getDetails();
+        e.displayDetails();
+    }
+}
